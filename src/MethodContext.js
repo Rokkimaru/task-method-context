40c67129f81a514cc24b1a62c50e0c80d7761f15
alexandr <frexeman2001@yandex.ru>
2022-03-14 13:01:21 +0300

    Задание task-method-context

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -19,7 +19,7 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {this.second(args.reverse());},
     second() {
         // здесь ничего писать не нужно
     },
